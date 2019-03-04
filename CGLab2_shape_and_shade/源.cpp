e4e95a6df8cfe15c3d4b181b7893a2eabc3a8eb9
birdmanmandbir <1522674529@qq.com>
2019-03-04 16:47:35 +0800

    final

--- CGLab2_shape_and_shade/源.cpp
+++ CGLab2_shape_and_shade/源.cpp
@@ -146,12 +146,45 @@ private:
 class SomeColorTriangle {
 public:
 	//points in order to make a triangle
-	float vertices[18] = {
+	float vertices[216] = {
 
 
-		0.0f, -0.5f, 0.0f,1.0f, 0.0f, 0.0f,
-		0.5f, -0.5f, 0.0f,0.0f, 1.0f, 0.0f,
-		0.5f,  0.5f, 0.0f,0.0f, 0.0f, 1.0f
+  -1.0f,-1.0f,-1.0f, 0.583f,  0.771f,  0.014f,// triangle 1 : begin
+	-1.0f,-1.0f, 1.0f,  0.609f,  0.115f,  0.436f,
+	-1.0f, 1.0f, 1.0f,  0.327f,  0.483f,  0.844f,// triangle 1 : end
+	1.0f, 1.0f,-1.0f, 0.822f,  0.569f,  0.201f, // triangle 2 : begin
+	-1.0f,-1.0f,-1.0f,    0.435f,  0.602f,  0.223f,
+	-1.0f, 1.0f,-1.0f,0.310f,  0.747f,  0.185f, // triangle 2 : end
+	1.0f,-1.0f, 1.0f, 0.597f,  0.770f,  0.761f,
+	-1.0f,-1.0f,-1.0f,0.559f,  0.436f,  0.730f,
+	1.0f,-1.0f,-1.0f,0.359f,  0.583f,  0.152f,
+	1.0f, 1.0f,-1.0f,0.483f,  0.596f,  0.789f,
+	1.0f,-1.0f,-1.0f,0.559f,  0.861f,  0.639f,
+	-1.0f,-1.0f,-1.0f,0.195f,  0.548f,  0.859f,
+	-1.0f,-1.0f,-1.0f,0.014f,  0.184f,  0.576f,
+	-1.0f, 1.0f, 1.0f,0.771f,  0.328f,  0.970f,
+	-1.0f, 1.0f,-1.0f,0.406f,  0.615f,  0.116f,
+	1.0f,-1.0f, 1.0f,0.676f,  0.977f,  0.133f,
+	-1.0f,-1.0f, 1.0f,0.971f,  0.572f,  0.833f,
+	-1.0f,-1.0f,-1.0f,0.140f,  0.616f,  0.489f,
+	-1.0f, 1.0f, 1.0f,0.997f,  0.513f,  0.064f,
+	-1.0f,-1.0f, 1.0f, 0.945f,  0.719f,  0.592f,
+	1.0f,-1.0f, 1.0f,0.543f,  0.021f,  0.978f,
+	1.0f, 1.0f, 1.0f, 0.279f,  0.317f,  0.505f, 
+	1.0f,-1.0f,-1.0f, 0.167f,  0.620f,  0.077f,
+	1.0f, 1.0f,-1.0f,0.347f,  0.857f,  0.137f,
+	1.0f,-1.0f,-1.0f,0.055f,  0.953f,  0.042f,
+	1.0f, 1.0f, 1.0f,0.714f,  0.505f,  0.345f,
+	1.0f,-1.0f, 1.0f,0.783f,  0.290f,  0.734f,
+	1.0f, 1.0f, 1.0f,0.722f,  0.645f,  0.174f,
+	1.0f, 1.0f,-1.0f,0.302f,  0.455f,  0.848f,
+	-1.0f, 1.0f,-1.0f,0.225f,  0.587f,  0.040f,
+	1.0f, 1.0f, 1.0f,0.517f,  0.713f,  0.338f,
+	-1.0f, 1.0f,-1.0f,0.053f,  0.959f,  0.120f,
+	-1.0f, 1.0f, 1.0f,0.393f,  0.621f,  0.362f,
+	1.0f, 1.0f, 1.0f,0.673f,  0.211f,  0.457f,
+	-1.0f, 1.0f, 1.0f,0.820f,  0.883f,  0.371f,
+	1.0f,-1.0f, 1.0f,0.982f,  0.099f,  0.879f
 
 	};
 	//生成顶点缓冲对象 ID:VBO
@@ -285,9 +318,9 @@ int main() {
 
 	// Camera matrix
 	glm::mat4 View = glm::lookAt(
-		glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
+		glm::vec3(4, 4, 4), // Camera is at (4,3,3), in World Space
 		glm::vec3(0, 0, 0), // and looks at the origin
-		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+		glm::vec3(2, -1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
 	);
 
 	// Model matrix : an identity matrix (model will be at the origin)
@@ -313,12 +346,12 @@ int main() {
 		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// 4. 当我们渲染一个物体时要使用着色器程序
-		glUseProgram(myTriangle.shaderProgram);
+		//// 4. 当我们渲染一个物体时要使用着色器程序
+		//glUseProgram(myTriangle.shaderProgram);
 
-		glBindVertexArray(myTriangle.VAO);
-		// 3. 绘制物体
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glBindVertexArray(myTriangle.VAO);
+		//// 3. 绘制物体
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
 		// 4. 当我们渲染一个物体时要使用着色器程序
 		glUseProgram(myscTriangle.shaderProgram);
 		// Send our transformation to the currently bound shader, in the "MVP" uniform
@@ -326,7 +359,7 @@ int main() {
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
 		glBindVertexArray(myscTriangle.VAO);
 		// 3. 绘制物体
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 3*12);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
