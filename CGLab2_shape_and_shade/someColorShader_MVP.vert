4f50821244e169c683d9b194555af974ab19c485
birdmanmandbir <1522674529@qq.com>
2019-04-02 22:38:07 +0800

    finish

--- CGLab2_shape_and_shade/someColorShader_MVP.vert
+++ CGLab2_shape_and_shade/someColorShader_MVP.vert
@@ -1,12 +1,13 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
-layout (location = 1) in vec3 aColor; // the color variable has attribute position 1
-  
-out vec3 ourColor; // output a color to the fragment shader
+layout (location = 1) in vec3 aNormal;
+out vec3 Normal;
+out vec3 FragPos;
 uniform mat4 MVP;
+uniform mat4 model;
 void main()
 {
-	//gl_Position = vec4(aPos, 1.0);
+    FragPos = vec3(model * vec4(aPos, 1.0));//ÎªÊ²Ã´ÊÇvec3
     gl_Position = MVP * vec4(aPos,1.0);
-    ourColor = aColor; // set ourColor to the input color we got from the vertex data
+	Normal = aNormal;
 }
--- CGLab2_shape_and_shade/源.cpp
+++ CGLab2_shape_and_shade/源.cpp
@@ -4,6 +4,7 @@
 #include <glad/glad.h>
 #include <glad/glad.c>
 #include <GLFW/glfw3.h>
+#include "shader_m.h"
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,12 +17,8 @@
 //#include "shaders.h"
 using namespace std;
 void processInput(GLFWwindow* window);
-char* readTheFile(string strSource);
-const char* fragmentShaderSource = readTheFile("shader.frag");
-const char* vertexShaderSource = readTheFile("shader.vert");
-const char* scfragmentShaderSource = readTheFile("someColorShader.frag");
-const char* scvertexShaderSource = readTheFile("someColorShader_MVP.vert");
-
+// lighting
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 void framebuffer_size_callback(GLFWwindow* windows, int width, int height) {
 	glViewport(0, 0, width, height);
@@ -32,255 +29,7 @@ void processInput(GLFWwindow* window) {
 		glfwSetWindowShouldClose(window, true);
 	}
 }
-//.frag .vert文件读取
-char* readTheFile(string strSource) {
-	std::ifstream myfile(strSource);
-	std::string str((std::istreambuf_iterator<char>(myfile)),
-		std::istreambuf_iterator<char>());
-	//str数组长度一定要 +1,
-	/*原因: https://blog.csdn.net/ShiQW5696/article/details/80676290 */
-	int len = str.length();
-	char* result = new char[len];
-	strcpy_s(result, len + 1, str.c_str());
-	return result;
-}
-class Triangle {
-public:
-	//points in order to make a triangle
-	float vertices[9] = {
-		-0.5f, -0.5f, 0.0f,
-		0.0f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
 
-	};
-	//生成顶点缓冲对象 ID:VBO
-	unsigned int VBO;
-	//生成顶点数组对象 ID:VAO
-	unsigned int VAO;
-	//储存 顶点着色器
-	unsigned int vertexShader;
-	//储存 片段着色器
-	unsigned int fragmentShader;
-	//存储 着色器程序
-	unsigned int shaderProgram;
-
-	void drawMyGraph() {
-		FragmentShaderInit();
-		vertexShaderInit();
-
-		shaderProgramLinker();
-		vertexInput();
-	}
-private:
-	void vertexInput() {
-		glGenBuffers(1, &VBO);
-		glGenVertexArrays(1, &VAO);
-
-		// 1. 绑定VAO , VBO
-		glBindVertexArray(VAO);
-
-		// 2. 复制顶点数组到缓冲中供OpenGL使用
-		//将缓冲对象 绑定到GL_ARRAY_BUFFER目标
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		//定义顶点数据复制到缓冲内存
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-		// 3. 设置顶点属性指针
-		//解析顶点数据
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		//启用顶点属性
-		glEnableVertexAttribArray(0);
-	}
-
-	void vertexShaderInit() {
-		//创建一个顶点着色器对象
-		vertexShader = glCreateShader(GL_VERTEX_SHADER);
-		//着色器源码附着到着色器对象上
-		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-		//编译着色器对象
-		glCompileShader(vertexShader);
-
-		//检测着色编译是否成功
-		int success;
-		char infoLog[50];
-		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-		if (!success) {
-			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
-	}
-
-	void FragmentShaderInit() {
-		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-		glCompileShader(fragmentShader);
-		//检测着色编译是否成功
-		int success;
-		char infoLog[50];
-		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-		if (!success) {
-			glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
-	}
-
-	void shaderProgramLinker() {
-		//创建着色器程序对象
-		shaderProgram = glCreateProgram();
-		//附加着色器到着色器程序
-		glAttachShader(shaderProgram, vertexShader);
-		glAttachShader(shaderProgram, fragmentShader);
-		glLinkProgram(shaderProgram);
-		int success;
-		char infoLog[50];
-		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-		if (!success) {
-			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::LINKE_PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
-		glDeleteShader(vertexShader);
-		glDeleteShader(fragmentShader);
-	}
-
-};
-class SomeColorTriangle {
-public:
-	//points in order to make a triangle
-	float vertices[216] = {
-
-
-  -1.0f,-1.0f,-1.0f, 0.583f,  0.771f,  0.014f,// triangle 1 : begin
-	-1.0f,-1.0f, 1.0f,  0.609f,  0.115f,  0.436f,
-	-1.0f, 1.0f, 1.0f,  0.327f,  0.483f,  0.844f,// triangle 1 : end
-	1.0f, 1.0f,-1.0f, 0.822f,  0.569f,  0.201f, // triangle 2 : begin
-	-1.0f,-1.0f,-1.0f,    0.435f,  0.602f,  0.223f,
-	-1.0f, 1.0f,-1.0f,0.310f,  0.747f,  0.185f, // triangle 2 : end
-	1.0f,-1.0f, 1.0f, 0.597f,  0.770f,  0.761f,
-	-1.0f,-1.0f,-1.0f,0.559f,  0.436f,  0.730f,
-	1.0f,-1.0f,-1.0f,0.359f,  0.583f,  0.152f,
-	1.0f, 1.0f,-1.0f,0.483f,  0.596f,  0.789f,
-	1.0f,-1.0f,-1.0f,0.559f,  0.861f,  0.639f,
-	-1.0f,-1.0f,-1.0f,0.195f,  0.548f,  0.859f,
-	-1.0f,-1.0f,-1.0f,0.014f,  0.184f,  0.576f,
-	-1.0f, 1.0f, 1.0f,0.771f,  0.328f,  0.970f,
-	-1.0f, 1.0f,-1.0f,0.406f,  0.615f,  0.116f,
-	1.0f,-1.0f, 1.0f,0.676f,  0.977f,  0.133f,
-	-1.0f,-1.0f, 1.0f,0.971f,  0.572f,  0.833f,
-	-1.0f,-1.0f,-1.0f,0.140f,  0.616f,  0.489f,
-	-1.0f, 1.0f, 1.0f,0.997f,  0.513f,  0.064f,
-	-1.0f,-1.0f, 1.0f, 0.945f,  0.719f,  0.592f,
-	1.0f,-1.0f, 1.0f,0.543f,  0.021f,  0.978f,
-	1.0f, 1.0f, 1.0f, 0.279f,  0.317f,  0.505f, 
-	1.0f,-1.0f,-1.0f, 0.167f,  0.620f,  0.077f,
-	1.0f, 1.0f,-1.0f,0.347f,  0.857f,  0.137f,
-	1.0f,-1.0f,-1.0f,0.055f,  0.953f,  0.042f,
-	1.0f, 1.0f, 1.0f,0.714f,  0.505f,  0.345f,
-	1.0f,-1.0f, 1.0f,0.783f,  0.290f,  0.734f,
-	1.0f, 1.0f, 1.0f,0.722f,  0.645f,  0.174f,
-	1.0f, 1.0f,-1.0f,0.302f,  0.455f,  0.848f,
-	-1.0f, 1.0f,-1.0f,0.225f,  0.587f,  0.040f,
-	1.0f, 1.0f, 1.0f,0.517f,  0.713f,  0.338f,
-	-1.0f, 1.0f,-1.0f,0.053f,  0.959f,  0.120f,
-	-1.0f, 1.0f, 1.0f,0.393f,  0.621f,  0.362f,
-	1.0f, 1.0f, 1.0f,0.673f,  0.211f,  0.457f,
-	-1.0f, 1.0f, 1.0f,0.820f,  0.883f,  0.371f,
-	1.0f,-1.0f, 1.0f,0.982f,  0.099f,  0.879f
-
-	};
-	//生成顶点缓冲对象 ID:VBO
-	unsigned int VBO;
-	//生成顶点数组对象 ID:VAO
-	unsigned int VAO;
-	//储存 顶点着色器
-	unsigned int vertexShader;
-	//储存 片段着色器
-	unsigned int fragmentShader;
-	//存储 着色器程序
-	unsigned int shaderProgram;
-
-	void drawMyGraph() {
-		FragmentShaderInit();
-		vertexShaderInit();
-
-		shaderProgramLinker();
-		vertexInput();
-	}
-private:
-	void vertexInput() {
-		glGenBuffers(1, &VBO);
-		glGenVertexArrays(1, &VAO);
-
-		// 1. 绑定VAO , VBO
-		glBindVertexArray(VAO);
-
-		// 2. 复制顶点数组到缓冲中供OpenGL使用
-		//将缓冲对象 绑定到GL_ARRAY_BUFFER目标
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		//定义顶点数据复制到缓冲内存
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-		// 3. 设置顶点属性指针
-		//解析顶点数据
-		//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		// position attribute
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
-		// color attribute
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-		glEnableVertexAttribArray(1);
-	}
-
-	void vertexShaderInit() {
-		//创建一个顶点着色器对象
-		vertexShader = glCreateShader(GL_VERTEX_SHADER);
-		//着色器源码附着到着色器对象上
-		glShaderSource(vertexShader, 1, &scvertexShaderSource, NULL);
-		//编译着色器对象
-		glCompileShader(vertexShader);
-
-		//检测着色编译是否成功
-		int success;
-		char infoLog[100];
-		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-		if (!success) {
-			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
-	}
-
-	void FragmentShaderInit() {
-		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fragmentShader, 1, &scfragmentShaderSource, NULL);
-		glCompileShader(fragmentShader);
-		//检测着色编译是否成功
-		int success;
-		char infoLog[100];
-		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-		if (!success) {
-			glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
-	}
-
-	void shaderProgramLinker() {
-		//创建着色器程序对象
-		shaderProgram = glCreateProgram();
-		//附加着色器到着色器程序
-		glAttachShader(shaderProgram, vertexShader);
-		glAttachShader(shaderProgram, fragmentShader);
-		glLinkProgram(shaderProgram);
-		int success;
-		char infoLog[50];
-		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-		if (!success) {
-			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::LINKE_PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
-		glDeleteShader(vertexShader);
-		glDeleteShader(fragmentShader);
-	}
-
-};
 
 //1
 int main() {
@@ -304,36 +53,90 @@ int main() {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-	//class
-	Triangle myTriangle;
-	myTriangle.drawMyGraph();
-	SomeColorTriangle myscTriangle;
-	myscTriangle.drawMyGraph();
-	//MVP
-	// Projection matrix : 45° Field of View, 4 : 3 ratio, display range : 0.1 unit < -> 100 units
-	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-
-	// Or, for an ortho camera :
-	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
-
-	// Camera matrix
-	glm::mat4 View = glm::lookAt(
-		glm::vec3(-4, -4, -4), // Camera is at (4,3,3), in World Space
-		glm::vec3(0, 0, 0), // and looks at the origin
-		glm::vec3(2, -1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-	);
-
-	// Model matrix : an identity matrix (model will be at the origin)
-	glm::mat4 Model = glm::mat4(1.0f);
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
 	// Enable depth test
 	glEnable(GL_DEPTH_TEST);
 	// Accept fragment if it closer to the camera than the former one
 	glDepthFunc(GL_LESS);
+
+
+	Shader lightingShader("someColorShader_MVP.vert","someColorShader.frag");
+	Shader lampShader("lamp.vs", "lamp.fs");
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+// ------------------------------------------------------------------
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+	};
+	// first, configure the cube's VAO (and VBO)
+	unsigned int VBO, cubeVAO;
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindVertexArray(cubeVAO);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// normal attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
+	// second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
+	unsigned int lightVAO;
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// note that we update the lamp's position attribute's stride to reflect the updated buffer data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 	// Get a handle for our "MVP" uniform
 	// Only during the initialisation
-	GLuint MatrixID = glGetUniformLocation(myscTriangle.shaderProgram, "MVP");
+	//GLuint MatrixID = glGetUniformLocation(myscTriangle.shaderProgram, "MVP");
 
 
 	//4
@@ -347,28 +150,52 @@ int main() {
 		processInput(window);
 
 		//渲染指令
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
 		glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
+		// be sure to activate shader when setting uniforms/drawing objects
+		lightingShader.use();
+		lightingShader.setVec3("objectColor", 0.33f, 1.0f, 0.62f);
+		lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+		lightingShader.setVec3("lightPos", lightPos);
+		//MVP
+// Projection matrix : 45° Field of View, 4 : 3 ratio, display range : 0.1 unit < -> 100 units
+		glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+		// Camera matrix
+		glm::mat4 View = glm::lookAt(
+			glm::vec3(-4, 4, -4), // Camera is at (4,3,3), in World Space
+			glm::vec3(0, 0, 0), // and looks at the origin
+			glm::vec3(2, -1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+		);
 
-		//// 4. 当我们渲染一个物体时要使用着色器程序
-		//glUseProgram(myTriangle.shaderProgram);
+		// Model matrix : an identity matrix (model will be at the origin)
+		glm::mat4 Model = glm::mat4(1.0f);
+		// Our ModelViewProjection : multiplication of our 3 matrices
+		glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
 
-		//glBindVertexArray(myTriangle.VAO);
-		//// 3. 绘制物体
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		// 4. 当我们渲染一个物体时要使用着色器程序
-		glUseProgram(myscTriangle.shaderProgram);
-		// Send our transformation to the currently bound shader, in the "MVP" uniform
-		// This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
-		glBindVertexArray(myscTriangle.VAO);
-		// 3. 绘制物体
-		glDrawArrays(GL_TRIANGLES, 0, 3*12);
+		lightingShader.setMat4("MVP", mvp);
+		lightingShader.setMat4("model",Model);
+		glBindVertexArray(cubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+
+		//// also draw the lamp object
+		//lampShader.use();
+		//lampShader.setMat4("projection", Projection);
+		//lampShader.setMat4("view", View);
+		//Model = glm::mat4(1.0f);
+		//Model = glm::translate(Model, lightPos);
+		//Model = glm::scale(Model, glm::vec3(0.2f)); // a smaller cube
+		//lampShader.setMat4("model", Model);
+
+		//glBindVertexArray(lightVAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 36);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-	glDeleteVertexArrays(1, &myTriangle.VAO);
-	glDeleteBuffers(1, &myTriangle.VBO);
+	//glDeleteVertexArrays(1, &myTriangle.VAO);
+	//glDeleteBuffers(1, &myTriangle.VBO);
 	glfwTerminate();
 	return 0;
 }
